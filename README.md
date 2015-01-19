c94181c6e32c35eb7fd9e9a773995dda61267cd2
AlexsJones <jonesax@hush.com>
2015-01-19 09:39:08 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+whisper
+=======
+
+
+Based on whisp this is a P2P domain level chat program that provides secure cryptography
+
+Components
+==========
+
+net/
+  Discovery Service
+  Multicast Service
+  Secure/Non-Secure Socket interface
+
+crypto/
+  Symmetrical key encrypt/decrypt
+  Asymmetrical key encrypt/decrypt
+  Integrity Service
+usr/
+  User command line I/O
+
---
+++ src/app/main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  Main launch point for whisper
+ *
+ *        Version:  1.0
+ *        Created:  19/01/15 09:36:16
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  AlexsJones (jonesax@hush.com), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+
+  return 0;
+}
