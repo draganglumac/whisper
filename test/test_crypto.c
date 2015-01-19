024b63f810744687da738237b61ac2862c3a89d3
AlexsJones <jonesax@hush.com>
2015-01-19 12:55:40 +0000

    wip

--- test/test_crypto.c
+++ test/test_crypto.c
@@ -20,18 +20,36 @@
 #include <jnxc_headers/jnxcheck.h>
 #include <jnxc_headers/jnxlog.h>
 
+void test_rsa_encryption() {
+
+  RSA *key = asymmetrical_generate_key(2048);
+  jnx_char *test_message = "Test";
+  jnx_int s = strlen(test_message); 
+
+  jnx_size olen,second_olen;
+  jnx_char *encrypted_message = asymmetrical_encrypt(key,test_message,&olen);
+
+  JNX_LOG(NULL,"Encrypted RSA length %d >%s<\n",olen,encrypted_message);
+
+  jnx_char *decrypted_message = asymmetrical_decrypt(key,encrypted_message,olen,&second_olen);
+
+  JNX_LOG(NULL,"Decrypted message %d >%s<\n",second_olen,decrypted_message);
+
+  JNXCHECK(strcmp(decrypted_message,test_message) == 0);
+}
 void test_rsa_key() {
   RSA *key = asymmetrical_generate_key(2048);
   JNXCHECK(key);
-  
+
   jnx_char *keystring = asymmetrical_key_to_string(key,PUBLIC);
 
   JNX_LOG(NULL,"Key \n%s",keystring);
-  
+  JNXCHECK(keystring);  
   jnx_char *keystringprivate = asymmetrical_key_to_string(key,PRIVATE);
 
   JNX_LOG(NULL,"Key \n%s",keystringprivate);
-  
+  JNXCHECK(keystringprivate);  
+
   free(keystring);
   free(keystringprivate);
   asymmetrical_destroy_key(key);
@@ -40,6 +58,6 @@ void test_rsa_key() {
 int main(int argc, char **argv) {
 
   test_rsa_key();
-
+  test_rsa_encryption();
   return 0;
 }
