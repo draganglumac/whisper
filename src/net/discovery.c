30a31e273368311e8bc0feb751811796e385e1b0
Dragan Glumac <dragan.glumac@gmail.com>
2015-01-20 16:15:09 +0000

    Adding files for config and discovery.

---
+++ src/net/discovery.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  discovery.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  20/01/15 16:14:20
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dragan Glumac (draganglumac), dragan.glumac@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "discovery.h"
+
