a11ce75a98804eea7c0ad0dbbea49604fcb3d209
AlexsJones <jonesax@hush.com>
2015-01-20 08:04:49 +0000

    starting work on the encoder

---
+++ src/integrity/encoding.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  encoding.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/20/2015 08:02:19 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "encoding.h"
+
+jnx_uint8 *encode_from_string(jnx_char *input, jnx_size input_length, jnx_size *output_length) {
+
+}
+
+jnx_uint8 *decode_to_string(jnx_char *input, jnx_size input_length, jnx_size *output_length) {
+
+}
