af3ed7b7ad8ae0d186de5b35317a515b899f8a30
AlexsJones <jonesax@hush.com>
2015-01-26 12:15:18 +0000

    updated cmake

---
+++ src/session/session.h
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  session.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  26/01/15 12:02:43
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef __SESSION_H__
+#define __SESSION_H__
+
+
+
+
+#endif
