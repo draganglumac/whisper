5e7770ba4f2bead603aa34823e0b17d62c027cdd
AlexsJones <jonesax@hush.com>
2015-02-24 08:43:02 -0800

    wip session

---
+++ src/net/communications.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  communications.c
+ *
+ *    Description:  communication 
+ *
+ *        Version:  1.0
+ *        Created:  02/24/2015 08:42:33 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  AlexsJones (jonesax@hush.com),
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
