6658ffd6b8253106bcefb37f42dde56ab4ebe319
Dragan Glumac <dragan.glumac@gmail.com>
2015-03-04 08:09:28 +0000

    Fixed a segfault when decrypt fails to retrieve the key, and also introduced JNXCHECKs for keys and secrets since strlen will segfault if you pass it a NULL pointer.

--- src/session/session.c
+++ src/session/session.c
@@ -8,16 +8,19 @@
 #include "session.h"
 
 void session_add_initiator_public_key(session *s, jnx_char *key) {
+  JNXCHECK(key);
   jnx_size len = strlen(key);
   s->initiator_public_key = malloc(len * sizeof(jnx_char));
   memcpy(s->initiator_public_key,key,len);
 }
 void session_add_receiver_public_key(session *s, jnx_char *key) {
+  JNXCHECK(key);
   jnx_size len = strlen(key);
   s->receiver_public_key = malloc(len * sizeof(jnx_char));
   memcpy(s->receiver_public_key,key,len);
 }
 void session_add_shared_secret(session *s, jnx_char *secret) {
+  JNXCHECK(secret);
   jnx_size len = strlen(secret);
   s->shared_secret = malloc(len * sizeof(jnx_char));
   memcpy(s->shared_secret,secret,len);
