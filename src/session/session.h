195e2d31e19187a50e10960c72fa87ae00e857f1
AlexsJones <jonesax@hush.com>
2015-03-06 03:09:59 -0800

    tidying in suession

--- src/session/session.h
+++ src/session/session.h
@@ -18,16 +18,7 @@
 #define __SESSION_H__
 #include "../crypto/cryptography.h"
 #include <jnxc_headers/jnxguid.h>
-
-typedef enum session_handshake_sender {
-  SENDER_AWAIT_PUB,
-  SENDER_AWAIT_FINISH
-}session_handshake_sender;
-
-typedef enum session_handshake_receiver {
-  RECEIVER_START,
-  RECEIVER_SHARED
-}session_handshake_receiver;
+#include <jnxc_headers/jnxqueue.h>
 
 typedef enum session_state {
   SESSION_STATE_OKAY,
@@ -43,13 +34,14 @@ typedef struct session {
   jnx_char *initiator_public_key;
   jnx_char *receiver_public_key;
   jnx_char *shared_secret;
+  jnx_int is_connected;
   /* local only */
   RSA *keypair;
-  jnx_int is_connected;
 }session;
 
 void session_add_initiator_public_key(session *s, jnx_char *key);
-void session_add_receiver_public_key(session *s, jnx_char *key);
-void session_add_shared_secret(session *s, jnx_char *secret);
 
+void session_add_receiver_public_key(session *s, jnx_char *key);
+
+void session_add_shared_secret(session *s, jnx_char *secret);
 #endif
