c3e4f2bde6ed8e940df30164c031125defed98f0
Dragan Glumac <dragan.glumac@gmail.com>
2015-03-15 13:08:05 +0000

    Changing how we quit for now, will have to automate it as a separate task.

--- src/app/app.c
+++ src/app/app.c
@@ -333,18 +333,22 @@ session *app_accept_chat(app_context_t *context) {
   jnx_unix_socket *us = jnx_unix_stream_socket_create(sockpath);
   jnx_unix_stream_socket_send(us, (jnx_uint8 *) "accept",
       strlen("accept"));
-  jnx_guid *session_guid = calloc(1, sizeof(jnx_guid));
-  read(us->socket, (void *) session_guid, sizeof(jnx_guid));
-  JNXCHECK(session_guid);
+  jnx_guid session_guid;
+  read(us->socket, (void *) &session_guid, sizeof(jnx_guid));
+#ifdef DEBUG
+  char *guid_str;
+  jnx_guid_to_string(&session_guid, &guid_str);
+  printf("[DEBUG] Received SessionGUID = %s\n", guid_str);
+  free(guid_str);
+#endif
   jnx_unix_socket_destroy(&us);
 
   session *osession = NULL;
   while (SESSION_STATE_NOT_FOUND 
       == session_service_fetch_session(context->session_serv,
-            session_guid, &osession)) {
+            &session_guid, &osession)) {
     sleep(1);
   }
-  free(session_guid);
   while (osession->secure_comms_fd == 0) {
     sleep(1);
   }
